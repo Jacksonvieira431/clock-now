3fd87a1cc3e01bca5b29b4c7366999497165bcb8
Jacksonvieira431 <jacksonhudson431@gmail.com>
2023-11-03 17:53:48 -0300

    Finaly

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from time import *
 
 
 window = Tk()
-window.title("Digital clock")
+window.title("Clock Now")
 window.geometry("420x150")
+window.resizable(1,1)
 
-
-
-
-
+l = Label(window,
+          font=("Arial", 68,"bold"),
+          bg="yellow",
+            fg="red",
+            bd=25)
+l.grid(row=0, column=1)
+def update():
+    t = strftime("%H:%M:%S")
+    l.config(text=t)
+    window.after(1000, update)
+update()
+    
 window.mainloop()
